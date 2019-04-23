99e1dfe831d60f6919df788c03edd48b698264e2
mmochizuki <mmocchi@pop07.odn.ne.jp>
2019-04-23 10:16:24 +0900

    テストファイルの追加

---
+++ agent2d_teest/AngleDeg.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include </home/mm/local/include/rcsc/geom/angle_deg.h>
+
+int main(int argc, char *argv[]){
+
+	rcsc::AngleDeg angle1( 100.0 );
+	rcsc::AngleDeg angle2( 30.0 );
+	rcsc::AngleDeg answer( 0 );
+
+	answer = angle1 + angle2;
+	std::cout <<"angle1:" <<angle1 << std::endl;
+	std::cout <<"angle2:" <<angle2 << std::endl;
+	std::cout <<"angle1+angle2=" <<answer << std::endl;
+
+	return 0;
+}
